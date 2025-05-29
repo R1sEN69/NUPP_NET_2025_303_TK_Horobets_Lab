335a76e1ffc0793b23dbf03569e3c21a23cfa174
R1sEN69 <129153896+R1sEN69@users.noreply.github.com>
2025-05-29 14:41:59 +0300

    Update VehicleExtensions.cs

--- Lab/Transport/Transport/Extensions/VehicleExtensions.cs
+++ Lab/Transport/Transport/Extensions/VehicleExtensions.cs
@@ -1,16 +1,13 @@
-﻿using Transport.Common.Models; // Потрібно для доступу до класу Vehicle
+﻿using Transport.Common.Models; 
 using System;
 
 namespace Transport.Common.Extensions
 {
-    // Клас для методів розширення повинен бути статичним
     public static class VehicleExtensions
     {
-        // Метод розширення: Додає функціонал для обчислення віку транспортного засобу
-        // "this Vehicle vehicle" вказує, що це метод розширення для типу Vehicle
+
         public static int CalculateAge(this Vehicle vehicle)
         {
-            // Метод розширення
             return DateTime.Now.Year - vehicle.Year;
         }
     }
